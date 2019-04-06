8ae81dd011c621a6118808353568bd84e2b77e04
axelghio <36314606+axelghio@users.noreply.github.com>
2019-04-06 13:35:32 -0300

    subir

--- RepasoArray/main.c
+++ RepasoArray/main.c
@@ -4,15 +4,5 @@
 
 int main()
 {
-    int sePudo=0;
-    int LaEdad;
-    if(dameEdadTres(&LaEdad,"dame tu edad ")==1)
-    {
-        printf("La edad es: %d", LaEdad);
-    }
-    else
-    {
-        printf("No se pudo.\n");
-    }
     return 0;
 }
